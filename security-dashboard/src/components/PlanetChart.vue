f494f3cc0a37fc86f81dac6d134c7382b5a4ca44
Mateo Espinosa <mateoespinosalopez@gmail.com>
2022-05-31 19:40:21 -0500

    adelanto a los charts

--- security-dashboard/src/components/PlanetChart.vue
+++ security-dashboard/src/components/PlanetChart.vue
@@ -9,5 +9,42 @@ import Chart from 'chart.js';
 
 export default {
 	name: 'PlanetChart',
+	extends: VueChartJs.Pie,
+
+	data() {
+		return {
+			etiquetas: [],
+			stock: [],
+			bgColors: [
+				'#ea80fc',
+				'#e040fb',
+				'#d500f9',
+				'#aa00ff',
+				'#f8bbd0',
+				'#f06292',
+				'#ec407a',
+				'#d81b60',
+				'#1de9b6',
+				'#00bfa5',
+				'#e65100',
+			],
+		};
+	},
+	mounted() {
+		this.mostrar();
+		this.renderChart(
+			{
+				labels: this.etiquetas,
+				datasets: [
+					{
+						label: 'Data One',
+						backgroundColor: this.bgColors,
+						data: this.stock,
+					},
+				],
+			},
+			{ responsive: true, maintainAspectRatio: false }
+		);
+	},
 };
 </script>
--- security-dashboard/src/views/DashboardView.vue
+++ security-dashboard/src/views/DashboardView.vue
@@ -59,5 +59,6 @@ export default {
 	components: {
 		PlanetChart,
 	},
+	props: ['userName', 'userType'],
 };
 </script>
