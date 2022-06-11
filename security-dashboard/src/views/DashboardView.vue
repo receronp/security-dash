dc6c960d2d73dc2e50f3dee1a9329a635e6d75d3
THeGratePapa <a01252280@itesm.mx>
2022-06-10 19:12:57 -0500

    subida de location svg

--- security-dashboard/src/views/DashboardView.vue
+++ security-dashboard/src/views/DashboardView.vue
@@ -1,95 +1,91 @@
 <template>
-	<div>
-		<h3>Unidades Activas</h3>
+  <div>
+    <h3>Unidades Activas</h3>
 
-		<div class="row">
-			<div class="column3">
-				<div class="cardn">
-					<img src="../assets/toDoList.svg" width="50px" height="50px" />
-						VAR_CASOS
-					<p>Promedio de casos diarios</p>
-				</div>
-			</div>
+    <div class="row">
+      <div class="column3">
+        <div class="cardn">
+          <img src="../assets/toDoList.svg" width="50px" height="50px" />
+          VAR_CASOS
+          <p>Promedio de casos diarios</p>
+        </div>
+      </div>
 
-			<div class="column3">
-				<div class="cardn">
-					<img src="../assets/reloj.svg" width="50px" height="50px" />
-							VAR_CASOS
-					<p>Tiempo promedio de respuesta</p>
-				</div>
-			</div>
+      <div class="column3">
+        <div class="cardn">
+          <img src="../assets/reloj.svg" width="50px" height="50px" />
+          VAR_CASOS
+          <p>Tiempo promedio de respuesta</p>
+        </div>
+      </div>
 
-			<div class="column3">		
-				<div class="cardn">
-					<img src="../assets/graph.svg" width="50px" height="50px" />
-						VAR_CASOS
-					<p>Mejora en tiempo de respuesta</p>
-				</div>
-			</div>
-		</div>
+      <div class="column3">
+        <div class="cardn">
+          <img src="../assets/graph.svg" width="50px" height="50px" />
+          VAR_CASOS
+          <p>Mejora en tiempo de respuesta</p>
+        </div>
+      </div>
+    </div>
 
-		<div class ="row">
-			<div class="column2">
-				<div class="row">	
-					<div class="card">	
-						Unidad VARIABLE_NUMERO_UNIDAD<br />
-						VARIABLE_LOCACION_UNIDAD<br />
-						VARIABLE_DESCRIPCION_UNIDAD<br />
-						VARIABLE_CALIFICACION_UNIDAD && VARIALBE_NUMERO_REVIEWS<br />
-					</div>
-				</div>
+    <div class="row">
+      <div class="column2">
+        <div class="row">
+          <div class="card">
+            Unidad VARIABLE_NUMERO_UNIDAD<br />
+            VARIABLE_LOCACION_UNIDAD<br />
+            VARIABLE_DESCRIPCION_UNIDAD<br />
+            VARIABLE_CALIFICACION_UNIDAD && VARIALBE_NUMERO_REVIEWS<br />
+          </div>
+        </div>
 
-				<div class="row">	
-					<div class="card">	
-						Unidad VARIABLE_NUMERO_UNIDAD<br />
-						VARIABLE_LOCACION_UNIDAD<br />
-						VARIABLE_DESCRIPCION_UNIDAD<br />
-						VARIABLE_CALIFICACION_UNIDAD && VARIALBE_NUMERO_REVIEWS<br />
-					</div>
-				</div>
-				<div class="row">	
-					<div class="card">	
-						Unidad VARIABLE_NUMERO_UNIDAD<br />
-						VARIABLE_LOCACION_UNIDAD<br />
-						VARIABLE_DESCRIPCION_UNIDAD<br />
-						VARIABLE_CALIFICACION_UNIDAD && VARIALBE_NUMERO_REVIEWS<br />
-					</div>
-				</div>
-			</div>
-			<div class="column2">
-				grafiquisima
-			</div>
-		</div>
+        <div class="row">
+          <div class="card">
+            Unidad VARIABLE_NUMERO_UNIDAD<br />
+            VARIABLE_LOCACION_UNIDAD<br />
+            VARIABLE_DESCRIPCION_UNIDAD<br />
+            VARIABLE_CALIFICACION_UNIDAD && VARIALBE_NUMERO_REVIEWS<br />
+          </div>
+        </div>
+        <div class="row">
+          <div class="card">
+            Unidad VARIABLE_NUMERO_UNIDAD<br />
+            VARIABLE_LOCACION_UNIDAD<br />
+            VARIABLE_DESCRIPCION_UNIDAD<br />
+            VARIABLE_CALIFICACION_UNIDAD && VARIALBE_NUMERO_REVIEWS<br />
+          </div>
+        </div>
+      </div>
+      <div class="column2">grafiquisima</div>
+    </div>
 
-		<div class="row">
-			<div class="column2">
-				<div class="cardn">
-					<img src="../assets/dashboard.svg" width="50px" height="50px" />
-						VAR_CASOS	
-					<div class="container">
-						<p>Promedio de casos diarios</p>
-					</div>
-				</div>
-			</div>
+    <div class="row">
+      <div class="column2">
+        <div class="cardn">
+          <img src="../assets/dashboard.svg" width="50px" height="50px" />
+          VAR_CASOS
+          <div class="container">
+            <p>Promedio de casos diarios</p>
+          </div>
+        </div>
+      </div>
 
-			<div class="column2">
-				<div class="cardn">
-					<img src="../assets/conference.svg" width="50px" height="50px" />
-						VAR_CASOS
-					<div class="container">
-						<p>Promedio de casos diarios</p>
-					</div>
-				</div>	
-			</div>
-		</div>			
-
-	</div>
+      <div class="column2">
+        <div class="cardn">
+          <img src="../assets/conference.svg" width="50px" height="50px" />
+          VAR_CASOS
+          <div class="container">
+            <p>Promedio de casos diarios</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-	props: ['userName', 'userType'],
+  props: ["userName", "userType"],
 };
 </script>
 
@@ -105,7 +101,7 @@ export default {
   padding: 10px 10px;
 }
 
-.column2{
+.column2 {
   float: left;
   width: 40%;
   padding: 10px 10px;
@@ -115,7 +111,7 @@ export default {
 
 /* Remove extra left and right margins, due to padding */
 .row {
-	margin: 0 -5px;
+  margin: 0 -5px;
 }
 
 /* Clear floats after the columns */
@@ -140,17 +136,16 @@ export default {
   padding: 16px;
   text-align: center;
   background-color: #ffffff;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   transition: 0.3s;
 }
 .cardn {
-  box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 8px;	
+  box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 8px;
   padding: 16px;
   text-align: center;
   background-color: #ffffff;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
 }
-
 
 .card:hover {
   box-shadow: rgba(17, 17, 26, 0.1) 0px 8px 16px;
