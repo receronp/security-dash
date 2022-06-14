fe437f31da5cd1de41e2eb9d1026c4c24295028b
Raul Ceron <receronp@gmail.com>
2022-06-14 16:33:14 -0500

    Adds registration verification of existing user.

--- security-backend/user.controller.js
+++ security-backend/user.controller.js
@@ -16,12 +16,21 @@ const login = async (req, res) => {
 const register = async (req, res) => {
     const { profile, name, username, password, user_vehicle } = req.body;
     const pool = await dataConn.getConnection();
-    const sql = "insert into users(username, password, name, profile, user_vehicle) values (?)";
-    const values = [ username, password, name, profile, user_vehicle ];
+    const sql1 = "select id, username, name, profile, user_vehicle from users where username = " + mysql.escape(username);
 
-    pool.query(sql, [values], function (err, result, fields) {
-        if (err) throw err;
-        return res.json(result);
+    pool.query(sql1, function (err1, result1) {
+        if (err1) throw err1;
+        if (result1.length == 0) {
+            const sql = "insert into users(username, password, name, profile, user_vehicle) values (?)";
+            const values = [ username, password, name, profile, user_vehicle ];
+        
+            pool.query(sql, [values], function (err, result) {
+                if (err) throw err;
+                return res.json(result);
+            });
+        } else {
+            return res.json(null);
+        }
     });
 }
 
--- security-dashboard/src/views/RegisterView.vue
+++ security-dashboard/src/views/RegisterView.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="mt-3">
-    <h1>Register</h1>
-    <div class="row">
+    <h1>Registrar Usuario</h1>
+    <div class="row mt-2">
       <div class="col-sm-3"></div>
       <div class="col-sm-2">
-        <label class="form-label text-end">Role:</label>
+        <label class="form-label text-end">Rol:</label>
       </div>
       <div class="col-sm-4 text-start">
         <select class="form-select" v-model="user.role">
-          <option disabled value="">Select role</option>
+          <option disabled value="">Selecciona Rol</option>
           <option value="admin">Admin</option>
-          <option value="employee">Employee</option>
+          <option value="employee">Empleado</option>
         </select>
       </div>
       <div class="col-sm-3"></div>
@@ -19,13 +19,13 @@
     <div class="row">
       <div class="col-sm-3"></div>
       <div class="col-sm-2">
-        <label class="form-label text-end">Name:</label>
+        <label class="form-label text-end">Nombre:</label>
       </div>
       <div class="col-sm-4 text-start">
         <input
           type="text"
           class="form-control"
-          placeholder="Name"
+          placeholder="Nombre"
           v-model="user.name"
         />
       </div>
@@ -35,13 +35,13 @@
     <div class="row">
       <div class="col-sm-3"></div>
       <div class="col-sm-2">
-        <label class="form-label text-end">Username:</label>
+        <label class="form-label text-end">Usuario:</label>
       </div>
       <div class="col-sm-4 text-start">
         <input
           type="text"
           class="form-control"
-          placeholder="Username"
+          placeholder="Usuario"
           v-model="user.username"
         />
       </div>
@@ -51,14 +51,30 @@
     <div class="row">
       <div class="col-sm-3"></div>
       <div class="col-sm-2">
-        <label class="form-label text-end">Password:</label>
+        <label class="form-label text-end">Contraseña:</label>
       </div>
       <div class="col-sm-4 text-start">
         <input
           type="password"
           class="form-control"
-          placeholder="Password"
+          placeholder="Contraseña"
           v-model="user.password"
+        />
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+    <br />
+    <div class="row">
+      <div class="col-sm-3"></div>
+      <div class="col-sm-2">
+        <label class="form-label text-end">ID de Unidad:</label>
+      </div>
+      <div class="col-sm-4 text-start">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="###"
+          v-model="user.user_vehicle"
           @keyup.enter="register()"
         />
       </div>
@@ -69,7 +85,7 @@
       <div class="col-sm-3"></div>
       <div class="col-sm-6 text-center">
         <button class="btn btn-primary" @click="register()">
-          Register User
+          Registrar Usuario
         </button>
       </div>
       <div class="col-sm-3"></div>
@@ -90,6 +106,7 @@ export default {
         username: "",
         role: "",
         password: "",
+        user_vehicle: null,
       },
     };
   },
@@ -100,24 +117,22 @@ export default {
         name: this.user.name,
         username: this.user.username,
         password: this.user.password,
+        user_vehicle: this.user.user_vehicle,
       };
 
       if (data.name && data.username && data.password && data.profile) {
         const result = await CallApi(urlBase + "/user/register", "POST", data);
-        if (result.length > 0) {
+        if (result !== null) {
           // Admin keeps his session active.
           // sessionStorage.setItem("sessionData", JSON.stringify(data));
           window.location.href = "index.html";
         } else {
-          if (data.profile.length == 0 || data.profile == null) {
-            alert("Invalid role selection.");
-          } else {
-            alert("Datos de registro no pudieron ser procesados.");
-          }
-          this.user.profile = "";
+          alert("Datos de registro no pudieron ser procesados.");
+          this.user.role = "";
           this.user.name = "";
           this.user.username = "";
           this.user.password = "";
+          this.user.user_vehicle = null;
         }
       } else {
         alert("Datos de registro incompletos.");
